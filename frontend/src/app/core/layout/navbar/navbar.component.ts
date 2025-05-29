36cb118c6fb01a1c4b1e20a76728b66584994006
Nguyen Quy <quynx9503.dev@gmail.com>
2025-05-29 12:15:22 +0700

    feat: Add Navbar component

--- frontend/src/app/core/layout/navbar/navbar.component.ts
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -1,13 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faHouse,
+  faSquarePlus,
+  faComments,
+  faShareFromSquare,
+  faFileCircleCheck,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  libIcon = inject(FaIconLibrary);
 
+  constructor() {
+    this.libIcon.addIcons(
+      faHouse,
+      faSquarePlus,
+      faComments,
+      faShareFromSquare,
+      faFileCircleCheck
+    );
+  }
 }
