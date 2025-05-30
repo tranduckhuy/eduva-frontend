b9933dc22650d41fd6921ecd7d0598160d42342e
huy01040104 <tronghuy01040@gmail.com>
2025-05-30 09:34:41 +0700

    feat: add setting dialog component

--- frontend/src/app/shared/pages/settings/personal/personal.component.ts
+++ frontend/src/app/shared/pages/settings/personal/personal.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ProfileCardComponent } from '../profile-card/profile-card.component';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-personal',
   standalone: true,
-  imports: [ProfileCardComponent],
+  imports: [ProfileCardComponent, DialogComponent],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PersonalComponent {}
+export class PersonalComponent {
+  openedDialog = signal<string | null>(null);
+
+  openDialog(type: string) {
+    this.openedDialog.set(type);
+  }
+
+  closeDialog() {
+    this.openedDialog.set(null);
+  }
+}
