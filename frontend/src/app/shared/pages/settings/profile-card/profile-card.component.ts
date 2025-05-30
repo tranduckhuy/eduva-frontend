167fbc43789f09036c320df48648e0d004d6c40f
huy01040104 <tronghuy01040@gmail.com>
2025-05-30 19:26:23 +0700

    refactor: split contact info and social media

--- frontend/src/app/shared/pages/settings/profile-card/profile-card.component.ts
+++ frontend/src/app/shared/pages/settings/profile-card/profile-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,4 +13,5 @@ export class ProfileCardComponent {
   title = input<string>('');
   subtitle = input<string | undefined>('');
   avatarUrl = input<string | undefined>('');
+  readonly = input<boolean>(false);
 }
