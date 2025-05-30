4794c1bb5fbc986b33aebcfe142774ad77849ddc
huy01040104 <tronghuy01040@gmail.com>
2025-05-30 11:04:45 +0700

    feat: add profile page

---
+++ frontend/src/app/shared/directives/text-expander/text-expander.directive.ts
@@ -0,0 +1,65 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+  HostListener,
+  OnInit,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appTextExpander]',
+  standalone: true,
+})
+export class TextExpanderDirective implements OnInit {
+  @Input('appTextExpander') maxLength = 100;
+  private originalText = '';
+  private isCollapsed = true;
+  private seeMoreBtn!: HTMLElement;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.originalText = this.el.nativeElement.textContent.trim();
+    if (this.originalText.length > this.maxLength) {
+      this.setCollapsedText();
+      this.addSeeMoreButton();
+    }
+  }
+
+  private setCollapsedText() {
+    const truncated = this.originalText.slice(0, this.maxLength) + '... ';
+    this.renderer.setProperty(this.el.nativeElement, 'textContent', truncated);
+  }
+
+  private setFullText() {
+    this.renderer.setProperty(
+      this.el.nativeElement,
+      'textContent',
+      this.originalText + ' '
+    );
+  }
+
+  private addSeeMoreButton() {
+    this.seeMoreBtn = this.renderer.createElement('span');
+    this.renderer.setStyle(this.seeMoreBtn, 'color', '#0056d6');
+    this.renderer.setStyle(this.seeMoreBtn, 'cursor', 'pointer');
+    this.renderer.setProperty(this.seeMoreBtn, 'textContent', 'Xem thêm');
+    this.renderer.appendChild(this.el.nativeElement, this.seeMoreBtn);
+
+    this.renderer.listen(this.seeMoreBtn, 'click', () => {
+      this.isCollapsed = !this.isCollapsed;
+      if (this.isCollapsed) {
+        this.setCollapsedText();
+        this.renderer.setProperty(this.seeMoreBtn, 'textContent', 'Xem thêm');
+      } else {
+        this.setFullText();
+        this.renderer.setProperty(this.seeMoreBtn, 'textContent', 'Thu gọn');
+      }
+      this.renderer.appendChild(this.el.nativeElement, this.seeMoreBtn);
+    });
+  }
+}
