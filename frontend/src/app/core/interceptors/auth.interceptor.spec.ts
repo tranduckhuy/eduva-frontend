cbb4b6ee18076b7e7490b697e9ee59945a22f9bb
Nguyen Quy <quynx9503.dev@gmail.com>
2025-06-02 23:58:58 +0700

    refactor: Refactor auth interceptor for more clear
    - Before: auth interceptor named api interceptor
    - After: Rename to auth interceptor for more close to logic implemented

--- frontend/src/app/core/interceptors/auth.interceptor.spec.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { apiInterceptor } from './api.interceptor';
+import { authInterceptor } from './auth.interceptor';
 
-describe('apiInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => apiInterceptor(req, next));
+describe('authInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) =>
+    TestBed.runInInjectionContext(() => authInterceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
