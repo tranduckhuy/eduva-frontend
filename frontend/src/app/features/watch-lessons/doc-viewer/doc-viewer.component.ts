58998fb7debcecbb921fea646b91963ce4612158
huy01040104 <tronghuy01040@gmail.com>
2025-07-01 17:35:10 +0700

    feat: add doc pdf audio viewer

---
+++ frontend/src/app/features/watch-lessons/doc-viewer/doc-viewer.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
+
+@Component({
+  selector: 'app-doc-viewer',
+  standalone: true,
+  imports: [CommonModule, NgxDocViewerModule],
+  templateUrl: './doc-viewer.component.html',
+  styleUrl: './doc-viewer.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DocViewerComponent {}
