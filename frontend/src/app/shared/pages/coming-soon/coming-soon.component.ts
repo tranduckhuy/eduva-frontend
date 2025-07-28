8b9933d11e5d9146b27fdeea04bca66fdf6fdca4
TrongHuy0104 <tronghuy0104@gmail.com>
2025-07-28 23:25:12 +0700

    fix: cannot redirect to home page

--- frontend/src/app/shared/pages/coming-soon/coming-soon.component.ts
+++ frontend/src/app/shared/pages/coming-soon/coming-soon.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-coming-soon',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ComingSoonComponent {
-
-}
+export class ComingSoonComponent {}
