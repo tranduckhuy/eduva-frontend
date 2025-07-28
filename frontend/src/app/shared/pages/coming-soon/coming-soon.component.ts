8a9e5086178ec72ecc459635097620ce68ecdb2e
TrongHuy0104 <tronghuy0104@gmail.com>
2025-07-28 23:23:19 +0700

    feat: add coming soon page

---
+++ frontend/src/app/shared/pages/coming-soon/coming-soon.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  standalone: true,
+  imports: [],
+  templateUrl: './coming-soon.component.html',
+  styleUrl: './coming-soon.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ComingSoonComponent {
+
+}
